4855f0e6b8cb7e5ea3c704199bd4bea74c42b901
Yoshinori Tanimura <tanimura@ricos.co.jp>
2020-12-05 19:41:30 +0900

    microfacet module の間のアンダーバーをハイフンにした

--- truck-rendimpl/src/shaders/face.frag
+++ truck-rendimpl/src/shaders/face.frag
@@ -1,6 +1,6 @@
 #version 450
 
-#include "microfacet_module.frag"
+#include "microfacet-module.frag"
 
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec2 uv;
--- truck-rendimpl/tests/microfacet.rs
+++ truck-rendimpl/tests/microfacet.rs
@@ -1,39 +1,8 @@
 mod common;
 use common::Plane;
-use glsl_to_spirv::ShaderType;
 use std::sync::{Arc, Mutex};
 use truck_platform::*;
-use truck_rendimpl::*;
 use wgpu::*;
-
-struct MFCheckPolygonInstance {
-    polygon: PolygonInstance,
-}
-
-impl Rendered for MFCheckPolygonInstance {
-    derive_get_set_id!(polygon);
-    derive_vertex_buffer!(polygon);
-    derive_bind_group_layout!(polygon);
-    derive_bind_group!(polygon);
-    #[inline(always)]
-    fn pipeline(
-        &self,
-        device_handler: &DeviceHandler,
-        layout: &PipelineLayout,
-    ) -> Arc<RenderPipeline>
-    {
-        let vertex_shader = include_str!("shaders/mesh-bindgroup.vert");
-        let vertex_spirv = common::compile_shader(vertex_shader, ShaderType::Vertex);
-        let vertex_module = wgpu::util::make_spirv(&vertex_spirv);
-        let mut fragment_shader = "#version 450\n\n".to_string();
-        fragment_shader += include_str!("../src/shaders/microfacet_module.frag");
-        fragment_shader += include_str!("shaders/check-mf-module.frag");
-        let fragment_spirv = common::compile_shader(&fragment_shader, ShaderType::Fragment);
-        let fragment_module = wgpu::util::make_spirv(&fragment_spirv);
-        self.polygon
-            .pipeline_with_shader(vertex_module, fragment_module, device_handler, layout)
-    }
-}
 
 #[test]
 fn microfacet_module_test() {
@@ -47,7 +16,7 @@ fn microfacet_module_test() {
     let texture = scene.device().create_texture(&tex_desc);
     
     let mut fragment_shader = "#version 450\n\n".to_string();
-    fragment_shader += include_str!("../src/shaders/microfacet_module.frag");
+    fragment_shader += include_str!("../src/shaders/microfacet-module.frag");
     fragment_shader += include_str!("shaders/check-mf-module.frag");
     let mut plane = Plane {
         vertex_shader: include_str!("shaders/plane.vert"),
@@ -56,4 +25,15 @@ fn microfacet_module_test() {
     };
     common::render_one(&mut scene, &texture, &mut plane);
     assert!(common::same_texture(scene.device_handler(), &answer, &texture));
+    
+    let mut fragment_shader = "#version 450\n\n".to_string();
+    fragment_shader += include_str!("../src/shaders/microfacet-module.frag");
+    fragment_shader += include_str!("shaders/anti-check-mf-module.frag");
+    let mut plane = Plane {
+        vertex_shader: include_str!("shaders/plane.vert"),
+        fragment_shader: &fragment_shader,
+        id: Default::default(),
+    };
+    common::render_one(&mut scene, &texture, &mut plane);
+    assert!(!common::same_texture(scene.device_handler(), &answer, &texture));
 }
