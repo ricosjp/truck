c6f31cc456406b360191129514580166893cadaf
Yoshinori Tanimura <tanimura@ricos.co.jp>
2021-01-21 23:27:36 +0900

    obj into matometa

--- truck-polymesh/tests/obj-io.rs
+++ truck-polymesh/tests/obj-io.rs
@@ -1,15 +1,15 @@
 use truck_polymesh::*;
 
-const TEAPOT_POSITION_OBJ: &[u8] = include_bytes!("teapot-position.obj");
-const TEAPOT_WITHNORMALS_OBJ: &[u8] = include_bytes!("teapot-with-normals.obj");
+const TEAPOT_POSITION_OBJ: &[u8] = include_bytes!("data/teapot-position.obj");
+const TEAPOT_WITHNORMALS_OBJ: &[u8] = include_bytes!("data/teapot-with-normals.obj");
 
 // https://sketchfab.com/3d-models/skull-downloadable-1a9db900738d44298b0bc59f68123393
 // Skull downloadable - CC Attribution © martinjario
-const SKULL_WITHTEXCOORD_OBJ: &[u8] = include_bytes!("skull-with-texcoord.obj");
+const SKULL_WITHTEXCOORD_OBJ: &[u8] = include_bytes!("data/skull-with-texcoord.obj");
 
 // https://sketchfab.com/3d-models/pony-cartoon-885d9f60b3a9429bb4077cfac5653cf9
 // Pony Cartoon - CC Attribution © Slava Z.
-const PONY_COMPLETE_OBJ: &[u8] = include_bytes!("pony-complete.obj");
+const PONY_COMPLETE_OBJ: &[u8] = include_bytes!("data/pony-complete.obj");
 
 #[test]
 fn position_obj_ioi_test() {
---
+++ truck-polymesh/tests/optimizing-test.rs
@@ -0,0 +1,6 @@
+use truck_polymesh::prelude::*;
+
+#[test]
+fn remove_unused_attrs_test() {
+    
+}
