735d29f3079c0505bc824fbf584851642428a384
Yoshinori Tanimura <tanimura@ricos.co.jp>
2021-04-15 18:25:12 +0900

    curve_in_surface

---
+++ truck-geometry/src/decorators/curve_in_surface.rs
@@ -0,0 +1,49 @@
+use crate::*;
+
+impl<C, S> CurveInSurface<C, S> {
+    /// Creates composited
+    #[inline(always)]
+    pub fn new(curve: C, surface: S) -> CurveInSurface<C, S> { CurveInSurface { curve, surface } }
+
+    /// Returns the reference to the previous map
+    #[inline(always)]
+    pub fn curve(&self) -> &C { &self.curve }
+
+    /// Returns the reference to the previous map
+    #[inline(always)]
+    pub fn surface(&self) -> &S { &self.surface }
+}
+
+impl<C, S> ParametricCurve for CurveInSurface<C, S>
+where
+    C: ParametricCurve<Point = Point2, Vector = Vector2>,
+    S: ParametricSurface,
+    S::Vector: VectorSpace<Scalar = f64>,
+{
+    type Point = S::Point;
+    type Vector = S::Vector;
+    #[inline(always)]
+    fn subs(&self, t: f64) -> Self::Point {
+        let pt = self.curve.subs(t);
+        self.surface.subs(pt[0], pt[1])
+    }
+    #[inline(always)]
+    fn der(&self, t: f64) -> Self::Vector {
+        let pt = self.curve.subs(t);
+        let der = self.curve.der(t);
+        self.surface.uder(pt[0], pt[1]) * der[0] + self.surface.vder(pt[0], pt[1]) * der[1]
+    }
+    #[inline(always)]
+    fn der2(&self, t: f64) -> Self::Vector {
+        let pt = self.curve.subs(t);
+        let der = self.curve.der(t);
+        let der2 = self.curve.der2(t);
+        self.surface.uuder(pt[0], pt[1]) * der[0] * der[0]
+            + self.surface.uvder(pt[0], pt[1]) * der[0] * der[1] * 2.0
+            + self.surface.vvder(pt[0], pt[1]) * der[1] * der[1]
+            + self.surface.uder(pt[0], pt[1]) * der2[0]
+            + self.surface.vder(pt[0], pt[1]) * der2[1]
+    }
+    #[inline(always)]
+    fn parameter_range(&self) -> (f64, f64) { self.curve.parameter_range() }
+}
