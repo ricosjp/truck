d28e78aff579ff25a2489c2b7764d8f9450eda5c
Yoshinori Tanimura <tanimura@ricos.co.jp>
2021-04-24 19:02:55 +0900

    test4extruded curve

--- truck-geometry/src/decorators/extruded_curve.rs
+++ truck-geometry/src/decorators/extruded_curve.rs
@@ -61,3 +61,55 @@ where C: ParametricCurve<Point = Point2, Vector = Vector2>
     #[inline(always)]
     fn normal(&self, _: f64, _: f64) -> C::Vector { C::Vector::zero() }
 }
+
+impl<C, V> BoundedSurface for ExtrudedCurve<C, V>
+where
+    C: ParametricCurve,
+    Self: ParametricSurface,
+{
+    fn parameter_range(&self) -> ((f64, f64), (f64, f64)) {
+        (self.curve.parameter_range(), (0.0, 1.0))
+    }
+}
+
+#[test]
+fn extruded_curve_test() {
+    let cpts = vec![
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.0, 1.0, 0.0),
+        Vector3::new(1.0, 0.0, 0.0),
+    ];
+    let spts = vec![
+        vec![
+            Vector3::new(0.0, 0.0, 0.0),
+            Vector3::new(0.0, 0.0, 1.0),
+        ],
+        vec![
+            Vector3::new(0.0, 1.0, 0.0),
+            Vector3::new(0.0, 1.0, 1.0),
+        ],
+        vec![
+            Vector3::new(1.0, 0.0, 0.0),
+            Vector3::new(1.0, 0.0, 1.0),
+        ],
+    ];
+    let curve = BSplineCurve::new(KnotVec::bezier_knot(2), cpts);
+    let surface0 = ExtrudedCurve::by_extrusion(curve, Vector3::unit_z());
+    let surface1 = BSplineSurface::new((KnotVec::bezier_knot(2), KnotVec::bezier_knot(1)), spts);
+    assert_eq!(surface0.parameter_range(), surface1.parameter_range());
+    
+    const N: usize = 10;
+    for i in 0..=N {
+        for j in 0..=N {
+            let u = i as f64 / N as f64;
+            let v = j as f64 / N as f64;
+            assert_near!(surface0.subs(u, v), ParametricSurface::subs(&surface1, u, v));
+            assert_near!(surface0.uder(u, v), surface1.uder(u, v));
+            assert_near!(surface0.vder(u, v), surface1.vder(u, v));
+            assert_near!(surface0.uuder(u, v), surface1.uuder(u, v));
+            assert_near!(surface0.uvder(u, v), surface1.uvder(u, v));
+            assert_near!(surface0.vvder(u, v), surface1.vvder(u, v));
+            assert_near!(surface0.normal(u, v), surface1.normal(u, v));
+        }
+    }
+}
