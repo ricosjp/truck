986a919488fde05f516d443d31947e8c54300feb
Yoshinori Tanimura <tanimura@ricos.co.jp>
2021-05-22 23:12:45 +0900

    remove dependency to rendimpl

--- truck-meshalgo/examples/tessellate_shape.rs
+++ truck-meshalgo/examples/tessellate_shape.rs
@@ -7,7 +7,7 @@ fn main() {
     let file = std::fs::File::open(&args[1]).unwrap();
     let solid = Solid::extract(serde_json::from_reader(file).unwrap()).unwrap();
     let mut poly =
-        tessellation::triangulation::tessellate_faces(solid.boundaries().iter().flatten(), 0.01)
+        tessellation::triangulation::tessellation(solid.boundaries().iter().flatten(), 0.01)
             .unwrap();
     poly.put_together_same_attrs().remove_unused_attrs();
     let mut string = Vec::<u8>::new();
