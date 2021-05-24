af80f39ed303d595416081f990f331bf0794a648
Yoshinori Tanimura <tanimura@ricos.co.jp>
2021-05-24 14:35:22 +0900

    prelude

--- truck-meshalgo/src/lib.rs
+++ truck-meshalgo/src/lib.rs
@@ -1,7 +1,14 @@
-pub use truck_polymesh::*;
+use truck_polymesh::*;
 use common::*;
 
 pub mod analyzers;
 mod common;
 pub mod filters;
 pub mod tessellation;
+
+pub mod prelude {
+    pub use truck_polymesh::*;
+    pub use crate::analyzers::*;
+    pub use crate::filters::*;
+    pub use crate::tessellation::*;
+}
