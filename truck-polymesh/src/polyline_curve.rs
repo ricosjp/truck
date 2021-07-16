8a5a56bcbf77ec3397f185236edd21cade79a407
Yoshinori Tanimura <tanimura@ricos.co.jp>
2021-07-16 19:35:56 +0900

    cut for polyline

--- truck-polymesh/src/polyline_curve.rs
+++ truck-polymesh/src/polyline_curve.rs
@@ -11,6 +11,14 @@ impl<P> DerefMut for PolylineCurve<P> {
 	fn deref_mut(&mut self) -> &mut Self::Target { &mut self.0 }
 }
 
+impl<P> From<Vec<P>> for PolylineCurve<P> {
+	fn from(v: Vec<P>) -> Self { Self(v) }
+}
+
+impl<P> From<PolylineCurve<P>> for Vec<P> {
+	fn from(v: PolylineCurve<P>) -> Self { v.0 }
+}
+
 impl<P: ControlPoint<f64>> ParametricCurve for PolylineCurve<P> {
 	type Point = P;
 	type Vector = P::Diff;
@@ -42,3 +50,68 @@ impl<P: ControlPoint<f64>> ParametricCurve for PolylineCurve<P> {
 	#[inline(always)]
 	fn der2(&self, _: f64) -> P::Diff { P::Diff::zero() }
 }
+
+impl<P: Clone> Invertible for PolylineCurve<P> {
+    #[inline(always)]
+    fn invert(&mut self) { self.reverse(); }
+    #[inline(always)]
+    fn inverse(&self) -> Self { Self(self.iter().rev().map(|p| p.clone()).collect()) }
+}
+
+impl<P: ControlPoint<f64>> Cut for PolylineCurve<P> {
+	fn cut(&mut self, t: f64) -> Self {
+		if t < 0.0 {
+			PolylineCurve(Vec::new())
+		} else if t + 1.0 > self.len() as f64 {
+			let mut v = Vec::new();
+			v.append(&mut self.0);
+			PolylineCurve(v)
+		} else {
+			let n = t as usize;
+			if t.near(&(n as f64)) {
+				let mut v = Vec::new();
+				v.extend(&self[n..]);
+				self.truncate(n + 1);
+				PolylineCurve(v)
+			} else {
+				let p = self.subs(t);
+				let mut v = vec![p];
+				v.extend(&self[(n + 1)..]);
+				self.truncate(n + 1);
+				self.push(p);
+				PolylineCurve(v)
+			}
+		}
+	}
+}
+
+#[test]
+fn polyline_test() {
+	let vec = vec![
+		Point3::new(0.0, 0.0, 0.0),
+		Point3::new(1.0, 0.0, 0.0),
+		Point3::new(0.0, 1.0, 0.0),
+		Point3::new(0.0, 0.0, 1.0),
+		Point3::new(0.0, 1.0, 1.0),
+		Point3::new(1.0, 0.0, 1.0),
+		Point3::new(1.0, 1.0, 0.0),
+		Point3::new(1.0, 1.0, 1.0),
+	];
+	let polyline = PolylineCurve(vec);
+	truck_base::assert_near!(polyline.subs(2.5), Point3::new(0.0, 0.5, 0.5));
+
+	let mut part0 = polyline.clone();
+	let part1 = part0.cut(2.5);
+	assert_eq!(part0.len(), 4);
+	assert_eq!(part1.len(), 6);
+	truck_base::assert_near!(part0.subs(2.5), Point3::new(0.0, 0.75, 0.25));
+	truck_base::assert_near!(part1.subs(0.5), Point3::new(0.0, 0.25, 0.75));
+	
+	let mut part0 = polyline.clone();
+	let part1 = part0.cut(4.0);
+	assert_eq!(part0.len(), 5);
+	assert_eq!(part1.len(), 4);
+	truck_base::assert_near!(part0.subs(2.5), Point3::new(0.0, 0.5, 0.5));
+	truck_base::assert_near!(part1.subs(0.5), Point3::new(0.5, 0.5, 1.0));
+}
+
