2d9d073e0cea6f3ef85fa8c114f12a04b08276de
Yoshinori Tanimura <tanimura@ricos.co.jp>
2021-09-07 17:14:48 +0900

    bug fix for failing when resized

--- truck-rendimpl/examples/app.rs
+++ truck-rendimpl/examples/app.rs
@@ -86,7 +86,7 @@ pub trait App: Sized + 'static {
             present_mode: PresentMode::Mailbox,
         };
 
-        let mut surface = unsafe { instance.create_surface(&window) };
+        let surface = unsafe { instance.create_surface(&window) };
         surface.configure(&device, &config);
 
         let handler = DeviceHandler::new(
@@ -108,7 +108,7 @@ pub trait App: Sized + 'static {
                     let frame = match surface.get_current_frame() {
                         Ok(frame) => frame,
                         Err(_) => {
-                            surface.configure(handler.device().as_ref(), &handler.config());
+                            surface.configure(handler.device(), &handler.config());
                             surface
                                 .get_current_frame()
                                 .expect("Failed to acquire next surface texture!")
@@ -126,7 +126,7 @@ pub trait App: Sized + 'static {
                         let mut config = handler.lock_config().unwrap();
                         config.width = size.width;
                         config.height = size.height;
-                        surface = unsafe { instance.create_surface(&window) };
+                        surface.configure(handler.device(), &config);
                         Self::default_control_flow()
                     }
                     WindowEvent::Moved(position) => app.moved(position),
