47dcb5d07f4f40020c5e291c14261b7103cb8446
Yoshinori Tanimura <tanimura@ricos.co.jp>
2021-09-28 17:58:26 +0900

    impl signed_crossing_faces

--- truck-meshalgo/src/analyzers/in_out_judge.rs
+++ truck-meshalgo/src/analyzers/in_out_judge.rs
@@ -57,6 +57,36 @@ fn random_normal_3d() -> Vector3 {
 
 /// whether a point is in a domain rounded by a closed polygon.
 pub trait IncludingPointInDomain {
+	/// Count signed number of faces crossing ray with origin `point` and direction `ray_direction`.
+	/// Counter increase if the dot product of the ray and the normal of a face is positive,
+	/// and decrease if it is negative.
+	/// 
+	/// # Examples
+	/// ```
+	/// use truck_meshalgo::prelude::*;
+	/// let positions = vec![
+	/// 	Point3::new(0.0, 0.0, 0.0),
+	/// 	Point3::new(1.0, 0.0, 0.0),
+	/// 	Point3::new(0.0, 1.0, 0.0),
+	/// 	Point3::new(0.0, 0.0, 1.0),
+	/// ];
+	/// let faces = Faces::from_iter(vec![
+	/// 	[0, 2, 1],
+	/// 	[0, 1, 3],
+	/// 	[0, 3, 2],
+	/// 	[1, 2, 3],
+	/// ]);
+	/// let simplex = PolygonMesh::new(
+	/// 	positions,
+	/// 	Vec::new(),
+	/// 	Vec::new(),
+	/// 	faces,
+	/// );
+	///
+	/// assert_eq!(simplex.signed_crossing_faces(Point3::new(0.1, 0.1, 0.1), Vector3::unit_x()), 1);
+	/// assert_eq!(simplex.signed_crossing_faces(Point3::new(-0.1, 0.1, 0.1), Vector3::unit_x()), 0);
+	/// ```
+	fn signed_crossing_faces(&self, point: Point3, ray_direction: Vector3) -> isize;
 	/// whether `point` is in a domain rounded by a closed polygon.
 	///
 	/// # Examples
@@ -88,16 +118,10 @@ pub trait IncludingPointInDomain {
 }
 
 impl IncludingPointInDomain for PolygonMesh {
-	fn inside(&self, point: Point3) -> bool {
-		let mut dir = random_normal_3d();
-		if dir.so_small() {
-			dir = random_normal_3d();
-		}
-		let dir = dir.normalize();
-		println!("{:?}", dir);
+	fn signed_crossing_faces(&self, point: Point3, ray_direction: Vector3) -> isize {
 		let ray = Ray {
 			origin: point,
-			direction: dir,
+			direction: ray_direction,
 		};
 		self.face_iter().fold(0, |mut counter, face| {
 			for i in 2..face.len() {
@@ -107,11 +131,18 @@ impl IncludingPointInDomain for PolygonMesh {
 					self.positions()[face[i].pos],
 				]);
 				if tri.is_crossing(ray) {
-					counter += f64::signum(tri.normal().dot(ray.direction)) as i32;
+					counter += f64::signum(tri.normal().dot(ray.direction)) as isize;
 				}
 			}
-			println!("{}", counter);
 			counter
-		}) >= 1
+		})
+	}
+	fn inside(&self, point: Point3) -> bool {
+		let mut dir = random_normal_3d();
+		if dir.so_small() {
+			dir = random_normal_3d();
+		}
+		let dir = dir.normalize();
+		self.signed_crossing_faces(point, dir) >= 1
 	}
 }
