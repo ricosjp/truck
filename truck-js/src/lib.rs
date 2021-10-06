1bd5893bccb8fe3fcdf9a412f5ed8c53dd4fbdb5
Yoshinori Tanimura <tanimura@ricos.co.jp>
2021-10-06 23:21:07 +0900

    add lint

--- truck-js/src/lib.rs
+++ truck-js/src/lib.rs
@@ -1,3 +1,16 @@
+//! Wasm wrapper API for truck
+
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications
+)]
+
 mod utils;
 
 use derive_more::*;
@@ -11,4 +24,5 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 mod shape_wrappers;
 pub use shape_wrappers::{Vertex, Edge, Wire, Face, Shell, Solid, AbstractShape, IntoWasm};
+/// the building model utility API
 pub mod builder;
--- truck-js/src/shape_wrappers.rs
+++ truck-js/src/shape_wrappers.rs
@@ -1,22 +1,26 @@
 use crate::*;
 
+/// Shape before wrapped wasm
 pub trait IntoWasm: Sized {
-    type WasmStruct: From<Self>;
-    fn into_wasm(self) -> Self::WasmStruct { self.into() }
+    /// wasm wrapper struct
+    type WasmWrapper: From<Self>;
+    /// Into wasm wrapper
+    fn into_wasm(self) -> Self::WasmWrapper { self.into() }
 }
 
 macro_rules! toporedef {
     ($type: ident, $member: ident) => {
-        /// wrapper from `truck-modeling`
+        /// wasm shape wrapper
         #[wasm_bindgen]
         #[derive(Clone, Debug, From, Into, Deref, DerefMut, AsRef)]
         pub struct $type(truck_modeling::$type);
 
         impl IntoWasm for truck_modeling::$type {
-            type WasmStruct = $type;
+            type WasmWrapper = $type;
         }
         #[wasm_bindgen]
         impl $type {
+            /// upcast to abstract shape
             #[inline(always)]
             pub fn upcast(self) -> AbstractShape {
                 let mut res = AbstractShape::empty();
@@ -33,6 +37,7 @@ macro_rules! toporedef {
 
 toporedef!(Vertex, vertex, Edge, edge, Wire, wire, Face, face, Shell, shell, Solid, solid);
 
+/// abstract shape, effectively an enumerated type
 #[wasm_bindgen]
 #[derive(Clone, Debug)]
 pub struct AbstractShape {
@@ -59,43 +64,61 @@ impl AbstractShape {
 
 #[wasm_bindgen]
 impl AbstractShape {
+    /// whether `self` is vertex or not.
     #[inline(always)]
     pub fn is_vertex(self) -> bool { self.vertex.is_some() }
+    /// whether `self` is edge or not.
     #[inline(always)]
     pub fn is_edge(self) -> bool { self.edge.is_some() }
+    /// whether `self` is wire or not.
     #[inline(always)]
     pub fn is_wire(self) -> bool { self.wire.is_some() }
+    /// whether `self` is face or not.
     #[inline(always)]
     pub fn is_face(self) -> bool { self.face.is_some() }
+    /// whether `self` is shell or not.
     #[inline(always)]
     pub fn is_shell(self) -> bool { self.shell.is_some() }
+    /// whether `self` is solid or not.
     #[inline(always)]
     pub fn is_solid(self) -> bool { self.solid.is_some() }
+    /// downcast
     #[inline(always)]
     pub fn into_vertex(self) -> Option<Vertex> { self.vertex }
+    /// downcast
     #[inline(always)]
     pub fn into_edge(self) -> Option<Edge> { self.edge }
+    /// downcast
     #[inline(always)]
     pub fn into_wire(self) -> Option<Wire> { self.wire }
+    /// downcast
     #[inline(always)]
     pub fn into_face(self) -> Option<Face> { self.face }
+    /// downcast
     #[inline(always)]
     pub fn into_shell(self) -> Option<Shell> { self.shell }
+    /// downcast
     #[inline(always)]
     pub fn into_solid(self) -> Option<Solid> { self.solid }
 }
 
 impl AbstractShape {
+    /// downcast as reference
     #[inline(always)]
     pub fn as_vertex(&self) -> Option<&Vertex> { self.vertex.as_ref() }
+    /// downcast as reference
     #[inline(always)]
     pub fn as_edge(&self) -> Option<&Edge> { self.edge.as_ref() }
+    /// downcast as reference
     #[inline(always)]
     pub fn as_wire(&self) -> Option<&Wire> { self.wire.as_ref() }
+    /// downcast as reference
     #[inline(always)]
     pub fn as_face(&self) -> Option<&Face> { self.face.as_ref() }
+    /// downcast as reference
     #[inline(always)]
     pub fn as_shell(&self) -> Option<&Shell> { self.shell.as_ref() }
+    /// downcast as reference
     #[inline(always)]
     pub fn as_solid(&self) -> Option<&Solid> { self.solid.as_ref() }
 }
