49c72b082b40079a4e2596526789add8eef7ff72
Yoshinori Tanimura <tanimura@ricos.co.jp>
2021-12-10 23:54:22 +0900

    TrimmedCurve

---
+++ truck-geometry/src/decorators/trimmied_curve.rs
@@ -0,0 +1,47 @@
+use super::*;
+
+impl<C> TrimmedCurve<C> {
+	/// constructor
+	#[inline(always)]
+	pub const fn new(curve: C, range: (f64, f64)) -> Self { Self { curve, range } }
+	/// Returns the reference of non-trimmed curve
+	#[inline(always)]
+	pub const fn curve(&self) -> &C { &self.curve }
+	/// Returns the mutable reference of non-trimmed curve
+	#[inline(always)]
+	pub fn curve_mut(&mut self) -> &mut C { &mut self.curve }
+}
+
+impl<C: ParametricCurve> ParametricCurve for TrimmedCurve<C> {
+	type Point = C::Point;
+	type Vector = C::Vector;
+	#[inline(always)]
+	fn subs(&self, t: f64) -> Self::Point { self.curve.subs(t) }
+	#[inline(always)]
+	fn der(&self, t: f64) -> Self::Vector { self.curve.der(t) }
+	#[inline(always)]
+	fn der2(&self, t: f64) -> Self::Vector { self.curve.der2(t) }
+}
+
+impl<C: ParametricCurve> BoundedCurve for TrimmedCurve<C> {
+	#[inline(always)]
+	fn parameter_range(&self) -> (f64, f64) { self.range }
+}
+
+impl<C: SearchNearestParameter> SearchNearestParameter for TrimmedCurve<C> {
+	type Point = C::Point;
+	type Parameter = C::Parameter;
+	#[inline(always)]
+	fn search_nearest_parameter(&self, pt: C::Point, hint: Option<C::Parameter>, trials: usize) -> Option<C::Parameter> {
+		self.curve.search_nearest_parameter(pt, hint, trials)
+	}
+}
+
+impl<C: SearchParameter> SearchParameter for TrimmedCurve<C> {
+	type Point = C::Point;
+	type Parameter = C::Parameter;
+	#[inline(always)]
+	fn search_parameter(&self, pt: C::Point, hint: Option<C::Parameter>, trials: usize) -> Option<C::Parameter> {
+		self.curve.search_parameter(pt, hint, trials)
+	}
+}
