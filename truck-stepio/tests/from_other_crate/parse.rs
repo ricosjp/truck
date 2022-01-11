320c0358eb9cd11e8f17036760eaf7e8f672433e
Yoshinori Tanimura <tanimura@ricos.co.jp>
2022-01-11 14:28:05 +0900

    tmp: mesh-torus

--- truck-stepio/tests/from_other_crate/parse.rs
+++ truck-stepio/tests/from_other_crate/parse.rs
@@ -20,17 +20,19 @@ DATA;
   #10 = VECTOR(#3, #8, LENGTH_MEASURE((6.0)));
   #11 = PLACEMENT(#3, #5);
   #12 = PLACEMENT(#3, #6);
-  #13 = AXIS_2_PLACEMENT_2D(#3, #7);
+  #13 = AXIS_2_PLACEMENT_2D(#11, $);
+  #14 = AXIS_2_PLACEMENT_3D(#12, $, $);
+  #15 = SURFACE(#3);
+  #16 = ELEMENTARY_SURFACE(#15, #14);
+  #17 = TOROIDAL_SURFACE(#16, POSITIVE_LENGTH_MEASURE((LENGTH_MEASURE((5.0)))), POSITIVE_LENGTH_MEASURE((LENGTH_MEASURE((2.0)))));
 ENDSEC;
 "#;
-  //#13 = AXIS_2_PLACEMENT_2D(#11, #7);
 
 fn test<'a, THolder, U>(idx: u64, answer: U)
 where
     THolder: Holder<Table = ap04x::Tables> + Deserialize<'a> + Debug + 'a,
     U: From<THolder::Owned> + Debug + PartialEq,
-    ap04x::Tables: EntityTable<THolder>,
-{
+    ap04x::Tables: EntityTable<THolder>, {
     let table = ap04x::Tables::from_str(REPRESENTATION_ITEM).unwrap();
     let a = EntityTable::<THolder>::get_owned(&table, idx).unwrap();
     assert_eq!(U::from(a), answer);
@@ -40,8 +42,7 @@ fn try_test<'a, THolder, U>(idx: u64, answer: U)
 where
     THolder: Holder<Table = ap04x::Tables> + Deserialize<'a> + Debug + 'a,
     U: TryFrom<THolder::Owned, Error = ExpressParseError> + Debug + PartialEq,
-    ap04x::Tables: EntityTable<THolder>,
-{
+    ap04x::Tables: EntityTable<THolder>, {
     let table = ap04x::Tables::from_str(REPRESENTATION_ITEM).unwrap();
     let a = EntityTable::<THolder>::get_owned(&table, idx).unwrap();
     assert_eq!(U::try_from(a).unwrap(), answer);
@@ -57,7 +58,22 @@ fn primitives() {
     test::<ap04x::DirectionHolder, Vector3>(8, Vector3::new(0.0, 0.0, 1.0));
     test::<ap04x::VectorHolder, Vector2>(9, Vector2::new(0.0, 6.0));
     test::<ap04x::VectorHolder, Vector3>(10, Vector3::new(0.0, 0.0, 6.0));
-	test::<ap04x::PlacementHolder, Point2>(11, Point2::new(1.0, 2.0));
-	test::<ap04x::PlacementHolder, Point3>(12, Point3::new(1.0, 2.0, 3.0));
-	test::<ap04x::Axis2Placement2DHolder, Matrix3>(13, Matrix3::new(0.0, 1.0, 0.0, -1.0, 0.0, 0.0, 1.0, 2.0, 1.0));
+    test::<ap04x::PlacementHolder, Point2>(11, Point2::new(1.0, 2.0));
+    test::<ap04x::PlacementHolder, Point3>(12, Point3::new(1.0, 2.0, 3.0));
+    test::<ap04x::Axis2Placement2DHolder, Matrix3>(
+        13,
+        Matrix3::from_translation(Vector2::new(1.0, 2.0)),
+    );
+}
+
+#[test]
+fn mesh_torus() {
+    use std::f64::consts::PI;
+    use truck_polymesh::{obj, StructuredMesh};
+    let table = ap04x::Tables::from_str(REPRESENTATION_ITEM).unwrap();
+    let step_torus = EntityTable::<ap04x::ToroidalSurfaceHolder>::get_owned(&table, 17).unwrap();
+    let truck_torus = ToroidalSurface::from(&step_torus).inverse();
+    let mesh = StructuredMesh::from_surface(&truck_torus, ((0.0, 2.0 * PI), (0.0, 2.0 * PI)), 0.05)
+        .destruct();
+    obj::write(&mesh, std::fs::File::create("torus-from-step.obj").unwrap()).unwrap();
 }
