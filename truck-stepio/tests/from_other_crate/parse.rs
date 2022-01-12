88da8605434b6dfb9c001ceddea727200749a85b
Yoshinori Tanimura <tanimura@ricos.co.jp>
2022-01-12 21:22:43 +0900

    recover

--- truck-stepio/tests/from_other_crate/parse.rs
+++ truck-stepio/tests/from_other_crate/parse.rs
@@ -6,7 +6,7 @@ use std::fmt::Debug;
 use std::str::FromStr;
 use truck_stepio::alias::*;
 
-const REPRESENTATION_ITEM: &str = r#"
+const STEP_CODE: &str = r#"
 DATA;
   #1 = LABEL('');
   #2 = REPRESENTATION_ITEM(#1);
@@ -28,52 +28,40 @@ DATA;
 ENDSEC;
 "#;
 
-fn test<'a, THolder, U>(idx: u64, answer: U)
+fn test<'a, THolder, U>(table: &ap04x::Tables, idx: u64, answer: U)
 where
     THolder: Holder<Table = ap04x::Tables> + Deserialize<'a> + Debug + 'a,
     U: From<THolder::Owned> + Debug + PartialEq,
     ap04x::Tables: EntityTable<THolder>, {
-    let table = ap04x::Tables::from_str(REPRESENTATION_ITEM).unwrap();
     let a = EntityTable::<THolder>::get_owned(&table, idx).unwrap();
     assert_eq!(U::from(a), answer);
 }
 
-fn try_test<'a, THolder, U>(idx: u64, answer: U)
+fn try_test<'a, THolder, U>(table: &ap04x::Tables, idx: u64, answer: U)
 where
     THolder: Holder<Table = ap04x::Tables> + Deserialize<'a> + Debug + 'a,
     U: TryFrom<THolder::Owned, Error = ExpressParseError> + Debug + PartialEq,
     ap04x::Tables: EntityTable<THolder>, {
-    let table = ap04x::Tables::from_str(REPRESENTATION_ITEM).unwrap();
     let a = EntityTable::<THolder>::get_owned(&table, idx).unwrap();
     assert_eq!(U::try_from(a).unwrap(), answer);
 }
 
 #[test]
 fn primitives() {
-    test::<ap04x::CartesianPointHolder, Point2>(5, Point2::new(1.0, 2.0));
-    test::<ap04x::CartesianPointHolder, Point3>(6, Point3::new(1.0, 2.0, 3.0));
-    try_test::<ap04x::PointAnyHolder, Point2>(5, Point2::new(1.0, 2.0));
-    try_test::<ap04x::PointAnyHolder, Point3>(6, Point3::new(1.0, 2.0, 3.0));
-    test::<ap04x::DirectionHolder, Vector2>(7, Vector2::new(0.0, 1.0));
-    test::<ap04x::DirectionHolder, Vector3>(8, Vector3::new(0.0, 0.0, 1.0));
-    test::<ap04x::VectorHolder, Vector2>(9, Vector2::new(0.0, 6.0));
-    test::<ap04x::VectorHolder, Vector3>(10, Vector3::new(0.0, 0.0, 6.0));
-    test::<ap04x::PlacementHolder, Point2>(11, Point2::new(1.0, 2.0));
-    test::<ap04x::PlacementHolder, Point3>(12, Point3::new(1.0, 2.0, 3.0));
+    let table = ap04x::Tables::from_str(STEP_CODE).unwrap();
+    test::<ap04x::CartesianPointHolder, Point2>(&table, 5, Point2::new(1.0, 2.0));
+    test::<ap04x::CartesianPointHolder, Point3>(&table, 6, Point3::new(1.0, 2.0, 3.0));
+    try_test::<ap04x::PointAnyHolder, Point2>(&table, 5, Point2::new(1.0, 2.0));
+    try_test::<ap04x::PointAnyHolder, Point3>(&table, 6, Point3::new(1.0, 2.0, 3.0));
+    test::<ap04x::DirectionHolder, Vector2>(&table, 7, Vector2::new(0.0, 1.0));
+    test::<ap04x::DirectionHolder, Vector3>(&table, 8, Vector3::new(0.0, 0.0, 1.0));
+    test::<ap04x::VectorHolder, Vector2>(&table, 9, Vector2::new(0.0, 6.0));
+    test::<ap04x::VectorHolder, Vector3>(&table, 10, Vector3::new(0.0, 0.0, 6.0));
+    test::<ap04x::PlacementHolder, Point2>(&table, 11, Point2::new(1.0, 2.0));
+    test::<ap04x::PlacementHolder, Point3>(&table, 12, Point3::new(1.0, 2.0, 3.0));
     test::<ap04x::Axis2Placement2DHolder, Matrix3>(
+        &table, 
         13,
         Matrix3::from_translation(Vector2::new(1.0, 2.0)),
     );
-}
-
-#[test]
-fn mesh_torus() {
-    use std::f64::consts::PI;
-    use truck_polymesh::{obj, StructuredMesh};
-    let table = ap04x::Tables::from_str(REPRESENTATION_ITEM).unwrap();
-    let step_torus = EntityTable::<ap04x::ToroidalSurfaceHolder>::get_owned(&table, 17).unwrap();
-    let truck_torus = ToroidalSurface::from(&step_torus).inverse();
-    let mesh = StructuredMesh::from_surface(&truck_torus, ((0.0, 2.0 * PI), (0.0, 2.0 * PI)), 0.05)
-        .destruct();
-    obj::write(&mesh, std::fs::File::create("torus-from-step.obj").unwrap()).unwrap();
 }
