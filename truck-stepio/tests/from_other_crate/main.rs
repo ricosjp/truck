33adfce52bcf5cba1883fe9c89e26fae8504d2e8
Yoshinori Tanimura <tanimura@ricos.co.jp>
2022-05-30 20:46:39 +0900

    add feature from_other_crate

--- truck-stepio/tests/from_other_crate/main.rs
+++ truck-stepio/tests/from_other_crate/main.rs
@@ -1,3 +1,5 @@
+#![cfg(feature = "from_other_crate")]
+
 pub mod config_control_design;
 truck_stepio::parse_primitives!(config_control_design, __parse_primitives);
 truck_stepio::impl_curve!(config_control_design, __impl_curve);
