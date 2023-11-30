3d222d38e7bd5845a4d6a52755e546bc5971af16
Yoshinori Tanimura <tanimura@ricos.co.jp>
2023-11-30 21:41:22 +0900

    add some options to step-to-mesh

--- truck-stepio/examples/shape-to-step.rs
+++ truck-stepio/examples/shape-to-step.rs
@@ -6,23 +6,28 @@
 //! shape-to-step <input shape file> [output shape file]
 //! ```
 
-use std::{cmp::Ordering, env};
+use clap::Parser;
 use truck_modeling::*;
 use truck_stepio::out;
 use truck_topology::compress::CompressedSolid;
 
-fn main() {
-    let mut args = env::args().collect::<Vec<_>>();
-    match args.len().cmp(&2) {
-        Ordering::Less => {
-            eprintln!("usage: shape-to-step <input shape file> [output shape file]");
-            return;
-        }
-        Ordering::Equal => args.push("output.stp".to_string()),
-        Ordering::Greater => {}
-    }
+/// convert from truck shape json to step file.
+#[derive(Parser, Debug)]
+struct Args {
+    /// file name of truck shape json
+    input_shape_file: String,
+    /// step file name
+    #[arg(default_value = "output.stp")]
+    output_step_file: String,
+}
 
-    let shape_file = std::fs::read(&args[1]).unwrap();
+fn main() {
+    let Args {
+        input_shape_file,
+        output_step_file,
+    } = Args::parse();
+
+    let shape_file = std::fs::read(&input_shape_file).unwrap();
     let compressed: CompressedSolid<Point3, Curve, Surface> =
         serde_json::from_reader(shape_file.as_slice()).unwrap();
     let step_string = out::CompleteStepDisplay::new(
@@ -33,7 +38,7 @@ fn main() {
         },
     )
     .to_string();
-    let mut step_file = std::fs::File::create(&args[2]).unwrap();
+    let mut step_file = std::fs::File::create(&output_step_file).unwrap();
     std::io::Write::write_all(&mut step_file, step_string.as_ref()).unwrap();
     let _ = ruststep::parser::parse(&step_string).unwrap();
 }
--- truck-stepio/examples/step-to-mesh.rs
+++ truck-stepio/examples/step-to-mesh.rs
@@ -1,23 +1,38 @@
 //! Parse STEP data, extract shape, and meshing.
 
-use std::{cmp::Ordering, path::Path};
+use clap::Parser;
+use std::path::Path;
 use truck_meshalgo::prelude::*;
 use truck_stepio::r#in::*;
 use truck_topology::compress::*;
 
+#[derive(Parser, Debug)]
+struct Args {
+    /// name of input step file
+    input_step_file: String,
+    /// name of output mesh file
+    #[arg(default_value = "output.obj")]
+    output_mesh_file: String,
+    /// output only edge file
+    /// Ignored when outputting obj files
+    #[arg(long("only-edge"))]
+    only_edge: bool,
+    /// output the condition of meshes
+    /// Ignored when outputting vtk files
+    #[arg(long("condition-check"))]
+    condition_check: bool,
+}
+
 fn main() {
-    let mut args = std::env::args().collect::<Vec<_>>();
-    match args.len().cmp(&2) {
-        Ordering::Less => {
-            eprintln!("usage: step-to-obj <input step file> [output obj file]");
-            return;
-        }
-        Ordering::Equal => args.push("output.obj".to_string()),
-        Ordering::Greater => {}
-    }
+    let Args {
+        input_step_file,
+        output_mesh_file,
+        only_edge,
+        condition_check,
+    } = Args::parse();
 
     println!("reading file...");
-    let step_file = std::fs::read_to_string(&args[1]).unwrap();
+    let step_file = std::fs::read_to_string(&input_step_file).unwrap();
     let exchange = ruststep::parser::parse(&step_file).unwrap();
     let table = Table::from_data_section(&exchange.data[0]);
     println!("meshing...");
@@ -30,11 +45,14 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let path: &Path = args[2].as_ref();
+    let path: &Path = output_mesh_file.as_ref();
     let extension = path.extension().and_then(|e| e.to_str());
     match extension {
-        Some("obj") => output_obj(&polyshells, path),
-        Some("vtu") => output_vtk(polyshells, path),
+        Some("obj") => output_obj(&polyshells, path, condition_check),
+        Some("vtu") => match only_edge {
+            true => output_vtk_edge(polyshells, path),
+            false => output_vtk(polyshells, path),
+        }
         _ => {}
     }
 }
@@ -42,11 +60,17 @@ fn main() {
 fn output_obj(
     polyshells: &[CompressedShell<Point3, PolylineCurve<Point3>, Option<PolygonMesh>>],
     path: &Path,
+    condition_check: bool,
 ) {
     let mut polymesh = PolygonMesh::default();
-    polyshells
-        .iter()
-        .for_each(|shell| polymesh.merge(shell.to_polygon()));
+    polyshells.iter().for_each(|shell| {
+        let mut poly = shell.to_polygon();
+        poly.put_together_same_attrs().remove_unused_attrs();
+        if condition_check {
+            println!("{:?}", poly.shell_condition());
+        }
+        polymesh.merge(poly);
+    });
     println!("output obj...");
     let obj_file = std::fs::File::create(path).unwrap();
     obj::write(&polymesh, obj_file).unwrap();
@@ -86,6 +110,48 @@ fn output_vtk(
                 pieces
             },
         )
+        .collect::<Vec<_>>();
+    let vtk = Vtk {
+        version: (1, 0).into(),
+        title: String::new(),
+        byte_order: ByteOrder::LittleEndian,
+        file_path: None,
+        data: DataSet::UnstructuredGrid { meta: None, pieces },
+    };
+
+    let obj_file = std::fs::File::create(path).unwrap();
+    vtk.write_xml(obj_file).unwrap();
+}
+
+fn output_vtk_edge(
+    polyshells: Vec<CompressedShell<Point3, PolylineCurve<Point3>, Option<PolygonMesh>>>,
+    path: &Path,
+) {
+    use vtkio::model::*;
+    let pieces = polyshells
+        .into_iter()
+        .flat_map(|shell| shell.edges)
+        .map(|CompressedEdge { curve, .. }| {
+            let points = curve
+                .0
+                .iter()
+                .copied()
+                .flat_map(Into::<[f64; 3]>::into)
+                .collect::<Vec<_>>();
+            let len = curve.0.len() as u64;
+            let connectivity = (0..len).chain(vec![0, len - 1]).collect();
+            Piece::Inline(Box::new(UnstructuredGridPiece {
+                points: IOBuffer::F64(points),
+                cells: Cells {
+                    cell_verts: VertexNumbers::XML {
+                        connectivity,
+                        offsets: vec![len, len + 1, len + 2],
+                    },
+                    types: vec![CellType::PolyLine, CellType::Vertex, CellType::Vertex],
+                },
+                data: Default::default(),
+            }))
+        })
         .collect::<Vec<_>>();
     let vtk = Vtk {
         version: (1, 0).into(),
