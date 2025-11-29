ecdb93fed9ebdc8d3d4d5a8038d23b9e193d4b2f
lzpel <18492524+lzpel@users.noreply.github.com>
2025-11-29 12:24:41 +0900

    update

--- truck-geometry/src/decorators/intersection_curve.rs
+++ truck-geometry/src/decorators/intersection_curve.rs
@@ -34,8 +34,13 @@ where
 	println!("a");
     let (z, w) = hint1.or_else(|| surface1.search_nearest_parameter(plane_point, hint1, trials))?;
 	println!("b v_0={:?} 次が収束しないことが問題", Vector4 { x, y, z, w });
-    let Vector4 { x, y, z, w } = newton::solve(function, Vector4 { x, y, z, w }, trials).ok()?;
-	println!("c");
+    let Vector4 { x, y, z, w } = match newton::solve(function, Vector4 { x, y, z, w }, trials) {
+		Ok(v) => v,
+		Err(e) => {
+			println!("Newton's method failed to converge: {}", e);
+			return None
+		}	
+	};
     let point = surface0.subs(x, y).midpoint(surface1.subs(z, w));
     Some((point, Point2::new(x, y), Point2::new(z, w)))
 }
