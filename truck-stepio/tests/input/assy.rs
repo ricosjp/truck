cfdce1031289f8cd6903aeed3eb4aab307436daf
Yoshinori Tanimura <tanimura@ricos.co.jp>
2025-12-11 18:27:47 +0900

    renewal dag

--- truck-stepio/tests/input/assy.rs
+++ truck-stepio/tests/input/assy.rs
@@ -1,4 +1,3 @@
-use truck_assembly::assy::*;
 use truck_stepio::r#in::*;
 
 const STEP_DIRECTORY: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../resources/step/");
@@ -7,9 +6,9 @@ const STEP_DIRECTORY: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../resources/
 fn occt_assy() {
     let step_string = std::fs::read_to_string([STEP_DIRECTORY, "occt-assy.step"].concat()).unwrap();
     let table = Table::from_step(&step_string).unwrap();
-    let assy = Assembly::new();
-    table.step_assy(&assy).unwrap();
-    let top = assy.top_nodes()[0];
-    let paths = top.maximal_paths_iter().collect::<Vec<_>>();
+    let assy = table.step_assy().unwrap();
+    assert_eq!(assy.len(), 3);
+    let top = assy.top_nodes().next().unwrap();
+    let paths = assy.maximal_paths_iter(top.index()).collect::<Vec<_>>();
     assert_eq!(paths.len(), 5);
 }
